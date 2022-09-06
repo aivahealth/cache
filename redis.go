42b553efa08075ed7e92900278c7310119201ee9
Joe Toscano <joe@aivainc.com>
2022-09-06 22:33:55 +0000

    Add readonly clients

--- redis.go
+++ redis.go
@@ -10,22 +10,39 @@ import (
 // TODO: generic cache interface in case we want to switch away from Redis
 
 var (
-	redisClient     *redis.Client
-	redisClientLock sync.Mutex
+	redisClientEvict     *redis.Client
+	redisClientEvictLock sync.Mutex
+
+	redisClientEvictReadonly     *redis.Client
+	redisClientEvictReadonlyLock sync.Mutex
 
 	redisClientNoevict     *redis.Client
 	redisClientNoevictLock sync.Mutex
+
+	redisClientNoevictReadonly     *redis.Client
+	redisClientNoevictReadonlyLock sync.Mutex
 )
 
 func RedisClientEvict() *redis.Client {
-	redisClientLock.Lock()
-	defer redisClientLock.Unlock()
-	if redisClient == nil {
-		redisClient = redis.NewClient(&redis.Options{
+	redisClientEvictLock.Lock()
+	defer redisClientEvictLock.Unlock()
+	if redisClientEvict == nil {
+		redisClientEvict = redis.NewClient(&redis.Options{
 			Addr: RedisAddressEvict(),
 		})
 	}
-	return redisClient
+	return redisClientEvict
+}
+
+func RedisClientEvictReadonly() *redis.Client {
+	redisClientEvictReadonlyLock.Lock()
+	defer redisClientEvictReadonlyLock.Unlock()
+	if redisClientEvictReadonly == nil {
+		redisClientEvictReadonly = redis.NewClient(&redis.Options{
+			Addr: RedisAddressEvictReadonly(),
+		})
+	}
+	return redisClientEvictReadonly
 }
 
 func RedisClientNoevict() *redis.Client {
@@ -39,6 +56,17 @@ func RedisClientNoevict() *redis.Client {
 	return redisClientNoevict
 }
 
+func RedisClientNoevictReadonly() *redis.Client {
+	redisClientNoevictReadonlyLock.Lock()
+	defer redisClientNoevictReadonlyLock.Unlock()
+	if redisClientNoevictReadonly == nil {
+		redisClientNoevictReadonly = redis.NewClient(&redis.Options{
+			Addr: RedisAddressNoevictReadonly(),
+		})
+	}
+	return redisClientNoevictReadonly
+}
+
 func RedisAddressEvict() string {
 	return os.Getenv("REDIS_DEDUP_ADDRESS")
 }
@@ -49,4 +77,12 @@ func RedisAddressNoevict() string {
 		maybeAddress = os.Getenv("REDIS_NOEVICT_ADDRESS")
 	}
 	return maybeAddress
+}
+
+func RedisAddressEvictReadonly() string {
+	return os.Getenv("REDIS_DEDUP_ADDRESS_READONLY")
+}
+
+func RedisAddressNoevictReadonly() string {
+	return os.Getenv("REDIS_NOEVICT_ADDRESS_READONLY")
 }
